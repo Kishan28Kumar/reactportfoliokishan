8c1aeadb29a92015fe449df5de3253c380ec25ae
kishan28kumar <kishankumar28032001@gmail.com>
2022-10-11 15:16:11 +0530

    work.js and workcardData.js completed

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -1,28 +1,26 @@
 import "./WorkCardStyles.css";
-import React from 'react'
+import React from "react";
 
-import proj1 from "../assets/proj1.avif"
 import { NavLink } from "react-router-dom";
 
-const WorkCard = () => {
+const WorkCard = (props) => {
   return (
-    <div className="work-container">
-        <h1 className="project-heading">Projects</h1>
-        <div className="project-container">
-            <div className="project-card">
-                <img src={proj1} alt=""/>
-                <h2 className="project-title">Project Title</h2>
-                <div className="pro-detail">
-                    <p>This is Text</p>
-                    <div className="pro-btns">
-                        <NavLink to="url.com" className="btn">View</NavLink>
-                        <NavLink to="url.com" className="btn">Source</NavLink>
-                    </div>
-                </div>
-            </div>
+    <div className="project-card">
+      <img src={props.imgsrc} alt="" />
+      <h2 className="project-title">{props.title}</h2>
+      <div className="pro-detail">
+        <p>{props.text}</p>
+        <div className="pro-btns">
+          <NavLink to={props.view} target="_blank" className="btn">
+            View
+          </NavLink>
+          <NavLink to="url.com" className="btn">
+            Source
+          </NavLink>
         </div>
-    </div> 
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default WorkCard
+export default WorkCard;
---
+++ src/components/WorkCardData.js
@@ -0,0 +1,27 @@
+import proj1 from "../assets/proj1.avif";
+import proj2 from "../assets/proj2.avif";
+import proj3 from "../assets/proj3.avif";
+
+const ProjectCardData = [
+    {
+        imgsrc: proj1,
+        title: "Ecommerce Website Design",
+        text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae corrupti possimus cupiditate quaerat obcaecati magnam ipsum, vel aperiam inventore nemo?",
+        view:"https://kishanreactportfolio.netlify.app/"
+    },
+    {
+        imgsrc: proj2,
+        title: "React Portfolio",
+        text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae corrupti possimus cupiditate quaerat obcaecati magnam ipsum, vel aperiam inventore nemo?",
+        view:"https://kishanportfolio-815e3.web.app/"
+    },
+    {
+        imgsrc: proj3,
+        title: "Text Manipulator",
+        text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae corrupti possimus cupiditate quaerat obcaecati magnam ipsum, vel aperiam inventore nemo?",
+        view:"https://text-manipulation.netlify.app/"
+    },
+]
+
+
+export default ProjectCardData;
